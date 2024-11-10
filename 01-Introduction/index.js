0646d930d27dee2d22c072581e177f904c979ef4
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-11-10 13:29:01 +0500

    hello-world-practice

--- 01-Introduction/index.js
+++ 01-Introduction/index.js
@@ -1,13 +1,34 @@
-console.log('hello backend')
-
 import express from "express"
-
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.get('/about', (req, res) => {
+  res.send('this is about section')
+})
+
+const userArray = [
+    {
+        id : new Date(),
+        name : 'haseeb ur rehman'
+    },
+    {
+        id : new Date(),
+        name : 'jawad akhtar'
+    },
+    {
+        id : new Date(),
+        name : 'ahmed'
+    },
+]
+
+app.get('/services', (req, res) => {
+  res.send('this is services section...')
+  res.Json(userArray)
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
