0f4675a11beaa659f47f11220c5bfb1de12efb91
haseeburrehmanjs <kamranking786500@gmail.com>
2024-11-17 19:31:31 +0500

    frontend-section-completed

--- Backend/index.js
+++ Backend/index.js
@@ -2,8 +2,24 @@ import express from 'express'
 const app = express()
 const port = 3000
 
+// middle wear
+app.use(express())
+
+// my first hello world api created
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+// defalut user created
+const user = [
+  {
+    id : new Date(),
+    userName : 'haseeb ur rehman'
+  }
+]
+
+app.get('/users', (req, res) => {
+  res.send(user)
 })
 
 app.listen(port, () => {
