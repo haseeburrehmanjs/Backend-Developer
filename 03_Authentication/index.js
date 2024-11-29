036b5493b336550fded135fdb29403d99cca03b1
haseeburrehmanjs <kamranking786500@gmail.com>
2024-11-29 22:11:53 +0500

    ubdated_work

--- 03_Authentication/index.js
+++ 03_Authentication/index.js
@@ -1,24 +1,12 @@
-// import express from 'express'
-// const app = express()
-// const port = 3000
-
-
-// app.get('/', (req, res) => {
-//     res.cookie('name', 'haseeb ur rehman js')
-//     res.send('done')
-// })
-
-// app.listen(`your port is live on local host 3000 ${port}`)
-
 import express from 'express'
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.cookie('name', 'haseeb')
-  res.send('done')
+    res.cookie('fullName', 'haseeb ur rehamn js')
+    res.send('cookies saved sucessfully')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
